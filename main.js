026101496807609757bd37909b6e7f037645f36b
mwakundia <brianmwakundia@gmail.com>
2024-03-06 10:25:43 +0300

    portfolio

---
+++ main.js
@@ -0,0 +1,59 @@
+// Toggle icon Navbar
+
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar')
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle("fa-xmark");
+    navbar.classList.toggle('active')
+}
+
+// Scroll section active link
+
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a')
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offset - 150;
+        let height  = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if (top >= offset && top < offset + height) {
+             navLinks.forEach.apply(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+             });
+        };
+ });
+
+//  sticky navbar
+let header = document.querySelector('header');
+header.classList.toggle('sticky', window.scrollY > 100);
+
+// Remove toggle icon and navbar
+menuIcon.classList.remove('fa-xmark');
+navbar.classList.remove('active');
+};
+
+// Scroll reveal
+
+ScrollReveal({
+     distance: '80px',
+     duration: 2000,
+     delay: 200,
+    });
+
+    ScrollReveal().reveal('.home-content, heading', { origin: 'top' });
+    ScrollReveal().reveal('.home-img, .services-container, .portfolio-box, .contact form', { origin: 'button' });
+    ScrollReveal().reveal('.home-contact h1, .about-img', { origin: 'left' });
+    ScrollReveal().reveal('.home-contact p, .about-content', { origin: 'right' });
+
+// Typed js
+const typed = new Typed('.mutiple-text', {
+    Strings: ['Software Developer & Designer'],
+    typeSpeed: 70,
+    backDelay: 1000,
+    loop: true, 
+});
